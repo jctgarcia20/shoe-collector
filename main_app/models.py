b3f1f3bcfbb2c9434bf9043ae4a8fb7fe410eb83
Jordan-Christopher Garcia <jctgarcia20@gmail.com>
2022-05-09 11:00:48 -0700

    Add Detail Functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Shoe(models.Model):
+  # First define the attributes/fields
+  name = models.CharField(max_length=100)
+  brand = models.CharField(max_length=100)
+  # Django will create inputs for a form
+  # TextField will create a <textarea>
+  description = models.CharField(max_length=100)
+  gender = models.CharField(max_length=100)
+  size = models.IntegerField(max_length=3)
+  color = models.CharField(max_length=100)
+  price = models.CharField(max_length=100)
+
+
+  def __str__(self):
+    return f'{self.name} ({self.id})'
--- main_app/views.py
+++ main_app/views.py
@@ -1,56 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Shoe
 
 # Create your views here.
 
-# Add the Cat class & list and view function below the imports
-class Shoe:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, brand, description, gender, size, color, price):
-    self.name = name
-    self.brand = brand
-    self.description = description
-    self.gender = gender
-    self.size = size
-    self.color = color
-    self.price = price
-
-shoes = [
-  Shoe(
-    'Air Jordan 1 High', 
-    'Air Jordan', 
-    'The One That Started It All', 
-    "Men's Shoes",
-    12, 
-    'Black/Red/White', 
-    '$170'
-    ),
-  Shoe(
-    'Nike Air Max 90', 
-    'Nike', 
-    'Nothing as fly, nothing as comfortable, nothing as proven—the Air Max 90 adds spring to your step. With fun-loving details from the mushroom on the heel to the butterfly on the insole, it delivers sunny vibes. Plus, it lets you do good by looking good.', 
-    "Men's Shoes",
-    11,
-    'Phantom/Iron Grey/Rush Pink/Celery',
-    '$140'
-    ),
-  Shoe(
-    "Nike Air Force 1 '07", 
-    'Nike', 
-    'Basic.', 
-    "Women's Shoes",
-    7,
-    'White',
-    '$100'
-    )
-]
-
 def home(request):
-  return HttpResponse('<h1>Hello World 👟</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def shoes_index(request):
+  shoes = Shoe.objects.all()
+  #                             data context |
   return render(request, 'shoes/index.html', {
-    'shoes': shoes
+    'shoes': shoes,
+  })
+
+def shoes_detail(request, shoe_id):
+  shoe = Shoe.objects.get(id=shoe_id)
+  return render(request, 'shoes/detail.html', {
+    'shoe': shoe 
   })
