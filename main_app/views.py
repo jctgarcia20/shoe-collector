57db2b44e96e071686677c7f2b295cb6083e608e
Jordan-Christopher Garcia <jctgarcia20@gmail.com>
2022-05-14 23:40:19 -0700

    Implement AWS and importing images functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Shoe, Occasion
+import os
+import uuid
+import boto3
+from .models import Shoe, Occasion, Photo
 from .forms import WoreForm
 
 # Create your views here.
@@ -88,3 +91,18 @@ class OccasionUpdate(UpdateView):
 class OccasionDelete(DeleteView):
   model = Occasion
   success_url = '/occasions/'
+
+def add_photo(request, shoe_id):
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+      bucket = os.environ['S3_BUCKET']
+      s3.upload_fileobj(photo_file, bucket, key)
+      url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+      Photo.objects.create(url=url, shoe_id=shoe_id)
+    except Exception as e:
+      print('An error occurred uploading file to S3')
+      print(e)
+  return redirect('detail', shoe_id=shoe_id)
